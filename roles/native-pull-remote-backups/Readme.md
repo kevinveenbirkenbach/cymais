9b09e5711bc05c005859f60ece1db38cd7596057
Kevin Veen-Birkenbach <kevin@veen.world>
2021-01-05 19:40:52 +0100

    Solved multiple bugs

--- roles/native-pull-remote-backups/Readme.md
+++ roles/native-pull-remote-backups/Readme.md
@@ -1,7 +1,14 @@
 # role native-pull-remote-backups
 
+## debug
+### live
 To track what the service is doing execute the following command:
 
 ```bash
   watch -n2 "systemctl status pull-remote-backups.service"
+```
+
+### history
+```bash
+  sudo journalctl -u pull-remote-backups
 ```
--- roles/native-pull-remote-backups/templates/pull-remote-backups.sh
+++ roles/native-pull-remote-backups/templates/pull-remote-backups.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 hosts="{{pull_remote_backups_hosts}}";
+errors=0
 for host in $hosts; do
-        bash /usr/local/bin/pull-remote-backup/pull-remote-backup.sh $host;
+        bash /usr/local/bin/pull-remote-backup/pull-remote-backup.sh $host || ((errors+=1));
 done;
+exit $errors;
