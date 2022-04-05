69bb10990ec94f307670945130f33ff970c6eef4
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2022-04-05 18:24:02 +0200

    Added exit code and regex

--- roles/native-btrfs-health-check/files/btrfs-health-check.sh
+++ roles/native-btrfs-health-check/files/btrfs-health-check.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# Checks the healt of all btrfs volumes
+exit_code=0
 for path in $(btrfs filesystem show | awk '/ path /{print $NF}')
 do
-  btrfs device stats $path
+  echo "Checking healt for $path..."
+  result=$(btrfs device stats $path)
+  echo "$result"
+  regex='\.(.*)_errs(\s*)[1-9]'
+  [[ "$result" =~ $regex ]] && echo "Errors found!" && exit_code=1;
 done
+exit $exit_code
