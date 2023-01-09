8150de75da6efc5f614f9236ea70f15af9c2e795
Kevin Veen-Birkenbach <kevin@veen.world>
2023-01-09 16:51:19 +0100

    Implemented excited check

--- roles/native-docker-health-check/files/docker-health-check.sh
+++ roles/native-docker-health-check/files/docker-health-check.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 docker_ps_grep_unhealthy="$(docker ps | grep unhealthy)"
-if [ -z "$docker_ps_grep_unhealthy" ]
+docker_ps_grep_exited="$(docker ps -a | grep Exited)"
+exitcode=0
+if [ ! -z "$docker_ps_grep_unhealthy" ]
 then 
-   echo "All docker containers are healthy."
-   exit
-else
    echo "Some docker containers are unhealthy: $docker_ps_grep_unhealthy"
-   exit 1
+   exitcode=1
 fi
+if [ ! -z "$docker_ps_grep_exited" ]
+then 
+   echo "Some docker containers exited: $docker_ps_grep_exited"
+   exitcode=2
+fi
+if [ "$exitcode" -ne "0" ]
+then 
+   exit $exitcode
+fi
+echo "All docker containers are healthy."
+exit
