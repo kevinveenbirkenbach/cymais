a4c3bcd6af859c553c1ed543d0cbe2a00259f73f
Kevin Veen-Birkenbach <kevin@veen.world>
2025-02-20 17:07:39 +0100

    Adapted realm and global styling

--- filter_plugins/configuration_filters.py
+++ filter_plugins/configuration_filters.py
@@ -5,6 +5,13 @@ def get_oauth2_enabled(applications, application_id):
     enabled = app.get('oauth2_proxy', {}).get('enabled', False)
     return bool(enabled)
 
+def get_oidc_enabled(applications, application_id):
+    # Retrieve the application dictionary based on the ID
+    app = applications.get(application_id, {})
+    # Retrieve the value for oidc.enabled, default is False
+    enabled = app.get('oidc', {}).get('enabled', False)
+    return bool(enabled)
+
 def get_css_enabled(applications, application_id):
     # Retrieve the application dictionary based on the given application_id.
     app = applications.get(application_id, {})
@@ -16,5 +23,6 @@ class FilterModule(object):
     def filters(self):
         return {
             'get_css_enabled': get_css_enabled,
+            'get_oidc_enabled': get_oidc_enabled,
             'get_oauth2_enabled': get_oauth2_enabled
         }
