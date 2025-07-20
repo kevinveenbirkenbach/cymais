1882fcfef53978ad7b251539a5812f0bcad7cf63
Kevin Veen-Birkenbach <kevin@veen.world>
2025-07-20 09:59:31 +0200

    Changed lam to web-app-lam

--- filter_plugins/redirect_filters.py
+++ filter_plugins/redirect_filters.py
@@ -20,7 +20,7 @@ class FilterModule(object):
           {{ redirect_list
                | add_redirect_if_group('lam',
                                        'ldap.' ~ primary_domain,
-                                       domains | get_domain('lam'),
+                                       domains | get_domain('web-app-lam'),
                                        group_names) }}
         """
         try:
